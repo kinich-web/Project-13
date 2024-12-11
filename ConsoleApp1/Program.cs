fd9bbc72a63a03b834c8662f5b1a3f481709c21b
kinich-web <ivanterzi96@gmail.com>
2024-12-12 00:05:34 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{/// <summary>
+/// 
+/// </summary>
+    internal class Program 
+    {/// <summary>
+     /// найти значения х
+     /// </summary>
+     /// <param name="args"></param>
+        public static double Func(int x)
+        {
+            return (Math.Sqrt(x) + x) / 2;
+        }
+        static void Main()
+        {
+            Console.WriteLine($"{ Func(5) + Func(12) + Func(19):f2}");
+            Console.ReadKey();
+        }
+
+        
+        
+
+    }
+}
---
+++ практикум 6/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Build.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практикум_6
+{/// <summary>
+/// Вычисление функции
+/// </summary>
+    internal class Program
+    {
+        public static int Input(String message)
+        { 
+        Console.Write(message);
+            return int.Parse(Console.ReadLine());
+            }
+        static double Func(int number)
+        {
+            return Math.Pow(number, 2) + 1;
+        }
+        static void Main(string[] args)
+        {
+            int number = Input("Введите число ");
+            Console.WriteLine($"f(x) = {Func(number)}");
+            Console.ReadKey();  
+        }
+    }
+}
